10a7ec4689314f14d9b70277907eebe45327f211
NachoCape <igcapezzolo@hotmail.com>
2023-01-08 22:56:58 +0000

    fix setup.sh

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,4 +1,5 @@
 apt-get update
+apt-get install wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.57.2/hugo_0.57.2_Linux-64bit.deb
 sudo dpkg -i hugo_0.57.2_Linux-64bit.deb
 apt-get install make
