4de4a3ba2bf8171d3d32fecf0f75ab806040476f
NachoCape <igcapezzolo@hotmail.com>
2023-01-08 22:52:43 +0000

    add task1

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,5 @@
-sudo apt-get update 
-apt-get install hugo 
+apt-get update
+wget https://github.com/gohugoio/hugo/releases/download/v0.57.2/hugo_0.57.2_Linux-64bit.deb
+sudo dpkg -i hugo_0.57.2_Linux-64bit.deb
 apt-get install make
 make build
