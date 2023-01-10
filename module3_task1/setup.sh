1f8c5076f8b91d3f3543b52a584f6ff65778e8ff
NachoCape <igcapezzolo@hotmail.com>
2023-01-10 19:48:53 +0000

    idk

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install wget
-sudo apt-get install make
-sudo wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_0.109.0_Linux-64bit.deb
-sudo dpkg -i hugo_0.109.0_Linux-64bit.deb
+apt-get update
+apt-get install wget
+apt-get install make
+wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
+dpkg -i hugo_0.79.0_Linux-64bit.deb
 exit 0
