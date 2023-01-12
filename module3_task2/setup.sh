ab6aae70e8439fab67652bac48739218bddc4dc8
NachoCape <igcapezzolo@hotmail.com>
2023-01-12 16:51:32 +0000

    .

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,7 +1,6 @@
 apt-get update
 apt-get install wget
 apt-get install make
-apt-get install npm
 npm install -g markdownlint-cli
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
